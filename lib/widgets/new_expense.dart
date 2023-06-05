bc3fb81c3882ffb670b974ecd612586d62589320
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2023-06-04 22:02:01 -0400

    Creating a new input field and a cancel button (initial)

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,10 +10,12 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -30,11 +32,27 @@ class _NewExpenseState extends State<NewExpense> {
               label: Text('Title'),
             ),
           ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              prefixText: '\$ ',
+              label: Text('Amount'),
+            ),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
           Row(
             children: [
+              TextButton(
+                onPressed: () {},
+                child: Text('Cancel'),
+              ),
               ElevatedButton(
                 onPressed: () {
-                  print(_titleController.text);
+                  print(
+                      "${_titleController.text} and ${_amountController.text}");
                 },
                 child: const Text('Save expense'),
               ),
