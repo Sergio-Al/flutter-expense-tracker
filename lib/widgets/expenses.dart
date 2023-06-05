bed63d7ec62e7a5154f3f642d9ce69663cd5c7a4
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2023-06-04 20:59:56 -0400

    Adding a Modal sheet & Understanding context
    Remember that context is inherited by the Stateful class and ctx is a context that Flutter provides to our builder function. Therefore, our widget function is returning a widget (Text)

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -26,13 +26,25 @@ class _ExpensesState extends State<Expenses> {
       category: Category.leisure,
     ),
   ];
+
+  void _openAddExpenseOverlay() {
+    // ...
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => Text('Modal Bottom sheet'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Expense Tracker'),
         actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
         ],
       ),
       body: Column(
