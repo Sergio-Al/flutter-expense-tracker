5b3f0025432a55401bf259bad329403f6515cd68
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2023-06-07 10:34:18 -0400

    Setting color scheme and text theme
    Flutter has different approaches to set the theme, but the most common is to apply them in the way that we are doing right now.
    In my opinion we should separate this piece of code in other file or bring them to a persistent data storage, even use it as a key value pair map object in order to be able to create our custom theme.

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,37 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 0, 35, 140));
+
 void main() {
   runApp(
     MaterialApp(
       theme: ThemeData().copyWith(
-          useMaterial3: true,
-          scaffoldBackgroundColor: Color.fromARGB(255, 224, 223, 255)),
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          // For styling buttons
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.normal,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 14,
+              ),
+            ),
+      ),
       home: const Expenses(),
     ),
   );
